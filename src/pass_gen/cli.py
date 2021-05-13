f4bf16a3fc1f30b01b62185eecdcd8d399e8e371
Aionmone <aionmone14@gmail.com>
2021-05-13 23:00:31 +0200

    Use click.IntRange see #1

--- src/pass_gen/cli.py
+++ src/pass_gen/cli.py
@@ -16,13 +16,10 @@ def version(ctx, param, value):
 @click.command()
 @click.option('-v', '--version', is_flag=True, callback=version,
                 expose_value=False, is_eager=True, help='Show version')
-@click.option('-l', '--length', type=int, default=12, metavar='', help='Password length [8 - 16]')
+@click.option('-l', '--length', type=click.IntRange(8, 16), default=12, metavar='', help='Password length [8 - 16]')
 def cli(length):
-    if length in range(8, 17):
-        password = pass_gen.generate(length)
-        click.echo(password)
-    else:
-        click.echo('Password length must be between 8-16.')
+    password = pass_gen.generate(length)
+    click.echo(password)
 
 if __name__ == '__main__':
     cli(12)
